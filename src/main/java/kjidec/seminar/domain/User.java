712c6b89442811300006f8bef00ea61ade1c54c5
KJI-DEC <kji_dec22@ewhain.net>
2022-04-10 18:21:56 +0900

    Add: userDomain

---
+++ src/main/java/kjidec/seminar/domain/User.java
@@ -0,0 +1,13 @@
+package kjidec.seminar.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String name;
+}
